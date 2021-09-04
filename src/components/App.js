f7ba840405616cd4627eef561c1882cfb8926f39
Godspower Eze <Godspowereze260@gmail.com>
2021-09-04 21:49:13 +0100

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,162 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import nft from '../nft.png';
 import './App.css';
+import Web3 from 'web3';
+import ConsensusQuizNft from "../abis/ConsensusQuizNFT.json"
+
+const CONTRACT_ADDRESS = "0x3310a4Bcd051EcdBFf67d3907aF8199D7C9399f7"
+
+const { create } = require('ipfs-http-client')
+
+const client = create({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https",
+});
+
+console.log(client)
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      "account": "",
+      "buffer":null,
+      "networkId":"",
+      "connected":false,
+      "contract":""
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        "buffer":Buffer(reader.result)
+      })
+    }
+  }
+
+   submitFormData = async (event) => {
+    event.preventDefault()
+    let { cid } = await client.add(this.state.buffer)
+    let hash = Object.values(Object.fromEntries(cid._baseCache))
+    let url = `https://ipfs.infura.io/ipfs/${hash}`
+    let nameValue = document.getElementById("name").value
+    let descValue = document.getElementById("description").value
+    let uuid = Math.random()
+    let jsonObject = {
+      "name":nameValue,
+      "description":descValue,
+      "image": url,
+      "uuid":uuid
+    }
+    fetch('https://nft-minter-api.herokuapp.com/api/add/', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jsonObject),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      this.state.contract.methods.mintNFT(`https://nft-minter-api.herokuapp.com/api/get_data/${uuid}`).send({
+        from:this.state.account
+      }).then(
+        (r)=>{
+          console.log(r)
+        }
+      )
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  async componentDidMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+      this.setState({
+        "connected":true
+      })
+    }else{
+      window.alert("Connect with Metamask")
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    const networkId = await web3.eth.net.getId()
+    this.setState({
+      "account":accounts[0],
+      "networkId":networkId
+    })
+    if(window.ethereum && networkId !== 4){
+      window.alert("Contract not deployed to this network. Switch to the Rinkeby TestNet")
+    }else{
+      const abi = ConsensusQuizNft.abi
+      const contract = await web3.eth.Contract(abi, CONTRACT_ADDRESS)
+      this.setState({
+        "contract":contract
+      })
+    }
+  }
   render() {
+    const renderMintButton = () => {
+      if (window.ethereum && this.state.networkId !== 4) {
+        return <input type='submit' value='Mint' disabled ></input>
+      } else {
+        return <input type='submit' value='Mint'></input>
+      }
+    }
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href="#"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={nft} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Your NFT Minter</h1>
+                <p><strong>Address:</strong> {this.state.account}</p>
+                <form onSubmit={this.submitFormData}> 
+                  <div>
+                    <label for="name"><b>Name</b></label>:
+                  </div>
+                  <div>
+                    <input type="text" placeholder="Enter Name" name="name" id="name" required></input>
+                  </div>
+                  <div>
+                  <label for="description"><b>Description</b></label>:
+                  </div>
+                  <div>
+                    <textarea name="description" placeholder="Enter Description" id="description" required></textarea>
+                  </div>
+                  <div>
+                    <label for="file_field"><b>Upload File</b></label>:
+                  </div>
+                  <div>
+                    <input type="file" placeholder="Enter Email" name="file_field" id="file_field" onChange={this.captureFile} required></input>
+                  </div>
+                  <div>
+                  {renderMintButton()}
+                  </div>
+                </form>
               </div>
             </main>
           </div>
